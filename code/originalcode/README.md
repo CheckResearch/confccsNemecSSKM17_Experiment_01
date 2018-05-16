9150771287e297c95919c361d06d2de4e0028efa
Ernst Kraaake <kermit737@t-online.de>
2018-05-16 12:51:25 +0200

    done.

--- code/originalcode/README.md
+++
@@ -1,5 +0,0 @@
-# Original Code
-
-Please put code originating from the publication in this directory.
-
-Additionally generated code belongs into the parent directory.
---
+++ code/originalcode/test_tls.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#from roca.detect import RocaFingerprinter, flatten, drop_none, AutoJSONEncoder
+#use local code instead
+from detect_tls import *
+import random
+import base64
+import unittest
+import pkg_resources
+
+
+__author__ = 'dusanklinec'
+
+
+class TlsTest(unittest.TestCase):
+    """TLS fingerprint test"""
+
+    def __init__(self, *args, **kwargs):
+        super(TlsTest, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        """
+        Loads testing certs
+        :return:
+        """
+
+    def tearDown(self):
+        """
+        Cleanup
+        :return:
+        """
+
+    def test_net(self):
+        """
+        Test university web - internet access
+        TODO: implement in a way internet access is not needed
+        :return:
+        """
+        tls_detect = RocaTLSFingerprinter()
+        res = tls_detect.process_tls('google.com', 'google.com')
+
+
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
+
+
